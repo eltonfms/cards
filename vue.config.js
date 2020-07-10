c1dbc529193ad6b78564cc4e38c358cadf6a4f5d
Elton Fagner <eltonfms@gmail.com>
2020-07-10 14:34:40 -0300

    styles / sass error vue 4

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+// vue.config.js
+/*module.exports = {
+  css: {
+    loaderOptions: {
+      scss: { 
+        prependData: `
+          @import "@/assets/sass/critical.scss";
+          @import "@/assets/sass/main.scss";
+        `
+      }, 
+    }
+  }
+};*/
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// webpack.config.js
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true,
+              sassOptions: {
+                indentedSyntax: true
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+}
